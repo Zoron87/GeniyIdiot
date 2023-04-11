f2e3b51e6cd5bea173f2f3323ca7d9cd0972a9af
Zoron87 <zoronbet@gmail.com>
2023-04-11 12:38:27 +0400

    Добавлено меню с возможностью рестарта, выхода из игры и просмотра правил игры

---
+++ 2048WinFormsApp/2048WinFormsApp/MainForm.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace _2048WinFormsApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.менюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.перезапуститьИгруToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(189, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Счет:";
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.scoreLabel.Location = new System.Drawing.Point(248, 4);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(23, 25);
+            this.scoreLabel.TabIndex = 1;
+            this.scoreLabel.Text = "0";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.менюToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(320, 33);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // менюToolStripMenuItem
+            // 
+            this.менюToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.перезапуститьИгруToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.менюToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.менюToolStripMenuItem.Name = "менюToolStripMenuItem";
+            this.менюToolStripMenuItem.Size = new System.Drawing.Size(77, 29);
+            this.менюToolStripMenuItem.Text = "Меню";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(256, 30);
+            this.toolStripMenuItem1.Text = "Правила игры";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // перезапуститьИгруToolStripMenuItem
+            // 
+            this.перезапуститьИгруToolStripMenuItem.Name = "перезапуститьИгруToolStripMenuItem";
+            this.перезапуститьИгруToolStripMenuItem.Size = new System.Drawing.Size(256, 30);
+            this.перезапуститьИгруToolStripMenuItem.Text = "Перезапустить игру";
+            this.перезапуститьИгруToolStripMenuItem.Click += new System.EventHandler(this.перезапуститьИгруToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(256, 30);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(320, 399);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "2048";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label scoreLabel;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem менюToolStripMenuItem;
+        private ToolStripMenuItem toolStripMenuItem1;
+        private ToolStripMenuItem перезапуститьИгруToolStripMenuItem;
+        private ToolStripMenuItem выходToolStripMenuItem;
+    }
+}
---
+++ 2048WinFormsApp/2048WinFormsApp/MainForm.cs
@@ -0,0 +1,301 @@
+namespace _2048WinFormsApp
+{
+    public partial class MainForm : Form
+    {
+        private const int mapSize = 4;
+        private Label[,] labelsMap;
+        private static Random random = new Random();
+        private int score = 0;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitMap();
+            GenerateNumber();
+            ShowScore();
+        }
+
+        private void ShowScore()
+        {
+            scoreLabel.Text = score.ToString();
+        }
+
+        private void InitMap()
+        {
+            labelsMap = new Label[mapSize, mapSize];
+
+            for (int i = 0; i < mapSize; i++)
+            {
+                for (int j = 0; j < mapSize; j++)
+                {
+                    var newLabel = CreateLabel(i, j, i*mapSize + j);
+                    Controls.Add(newLabel);
+                    labelsMap[i, j] = newLabel;
+                }
+            }
+        }
+
+        private void GenerateNumber()
+        {
+            while (true)
+            {
+                var randomNumberLabel = random.Next(mapSize * mapSize);
+                var indexRow = randomNumberLabel / mapSize;
+                var indexColumn = randomNumberLabel % mapSize;
+
+                if (labelsMap[indexRow, indexColumn].Text == string.Empty)
+                {
+                    labelsMap[indexRow, indexColumn].Text = "2";
+                    break;
+                }
+            }
+        }
+
+        private Label CreateLabel(int indexRow, int indexColumn, int number)
+        {
+            var label = new Label();
+
+            label.BackColor = SystemColors.ControlDark;
+            label.Font = new Font("Segoe UI", 18F, FontStyle.Bold,GraphicsUnit.Point);
+            label.Size = new Size(70, 70);
+            label.TextAlign = ContentAlignment.MiddleCenter;
+            int x = 10 + indexColumn * 76;
+            int y = 70 + indexRow * 76;
+            label.Location = new Point(x, y);
+
+            return label;
+        }
+
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                ArrowKeysRight();
+                GenerateNumber();
+            }
+
+            if (e.KeyCode == Keys.Left)
+            {
+                ArrowKeysLeft();
+                GenerateNumber();
+            }
+
+            if (e.KeyCode == Keys.Up)
+            {
+                ArrowKeysUp();
+                GenerateNumber();
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                ArrowKeysDown();
+                GenerateNumber();
+            }
+            ShowScore();
+        }
+
+        private void ArrowKeysDown()
+        {
+            for (int j = 0; j < mapSize; j++)
+            {
+                for (int i = mapSize - 1; i >= 0; i--)
+                {
+                    if (labelsMap[i, j].Text != String.Empty)
+                    {
+                        for (int k = i - 1; k >= 0; k--)
+                        {
+                            if (labelsMap[k, j].Text != String.Empty)
+                            {
+                                if (labelsMap[i, j].Text == labelsMap[k, j].Text)
+                                {
+                                    var number = int.Parse(labelsMap[i, j].Text);
+                                    score += number * 2;
+                                    labelsMap[i, j].Text = (number * 2).ToString();
+                                    labelsMap[k, j].Text = String.Empty;
+                                }
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            for (int j = 0; j < mapSize; j++)
+            {
+                for (int i = mapSize - 1; i >= 0; i--)
+                {
+                    if (labelsMap[i, j].Text == String.Empty)
+                    {
+                        for (int k = i - 1; k >= 0; k--)
+                        {
+                            if (labelsMap[k, j].Text != String.Empty)
+                            {
+                                labelsMap[i, j].Text = labelsMap[k, j].Text;
+                                labelsMap[k, j].Text = String.Empty;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void ArrowKeysUp()
+        {
+            for (int j = 0; j < mapSize; j++)
+            {
+                for (int i = 0; i < mapSize; i++)
+                {
+                    if (labelsMap[i, j].Text != String.Empty)
+                    {
+                        for (int k = i + 1; k < mapSize; k++)
+                        {
+                            if (labelsMap[k, j].Text != String.Empty)
+                            {
+                                if (labelsMap[i, j].Text == labelsMap[k, j].Text)
+                                {
+                                    var number = int.Parse(labelsMap[i, j].Text);
+                                    score += number * 2;
+                                    labelsMap[i, j].Text = (number * 2).ToString();
+                                    labelsMap[k, j].Text = String.Empty;
+                                }
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            for (int j = 0; j < mapSize; j++)
+            {
+                for (int i = 0; i < mapSize; i++)
+                {
+                    if (labelsMap[i, j].Text == String.Empty)
+                    {
+                        for (int k = i + 1; k < mapSize; k++)
+                        {
+                            if (labelsMap[k, j].Text != String.Empty)
+                            {
+                                labelsMap[i, j].Text = labelsMap[k, j].Text;
+                                labelsMap[k, j].Text = String.Empty;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void ArrowKeysLeft()
+        {
+            for (int i = 0; i < mapSize; i++)
+            {
+                for (int j = 0; j < mapSize; j++)
+                {
+                    if (labelsMap[i, j].Text != String.Empty)
+                    {
+                        for (int k = j + 1; k < mapSize; k++)
+                        {
+                            if (labelsMap[i, k].Text != String.Empty)
+                            {
+                                if (labelsMap[i, j].Text == labelsMap[i, k].Text)
+                                {
+                                    var number = int.Parse(labelsMap[i, j].Text);
+                                    score += number * 2;
+                                    labelsMap[i, j].Text = (number * 2).ToString();
+                                    labelsMap[i, k].Text = String.Empty;
+                                }
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < mapSize; i++)
+            {
+                for (int j = 0; j < mapSize; j++)
+                {
+                    if (labelsMap[i, j].Text == String.Empty)
+                    {
+                        for (int k = j + 1; k < mapSize; k++)
+                        {
+                            if (labelsMap[i, k].Text != String.Empty)
+                            {
+                                labelsMap[i, j].Text = labelsMap[i, k].Text;
+                                labelsMap[i, k].Text = String.Empty;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void ArrowKeysRight()
+        {
+            for (int i = 0; i < mapSize; i++)
+            {
+                for (int j = mapSize - 1; j >= 0; j--)
+                {
+                    if (labelsMap[i, j].Text != String.Empty)
+                    {
+                        for (int k = j - 1; k >= 0; k--)
+                        {
+                            if (labelsMap[i, k].Text != String.Empty)
+                            {
+                                if (labelsMap[i, j].Text == labelsMap[i, k].Text)
+                                {
+                                    var number = int.Parse(labelsMap[i, j].Text);
+                                    score += number * 2;
+                                    labelsMap[i, j].Text = (number * 2).ToString();
+                                    labelsMap[i, k].Text = String.Empty;
+                                }
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < mapSize; i++)
+            {
+                for (int j = mapSize - 1; j >= 0; j--)
+                {
+                    if (labelsMap[i, j].Text == String.Empty)
+                    {
+                        for (int k = j - 1; k >= 0; k--)
+                        {
+                            if (labelsMap[i, k].Text != String.Empty)
+                            {
+                                labelsMap[i, j].Text = labelsMap[i, k].Text;
+                                labelsMap[i, k].Text = String.Empty;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            GameRulesForm gameRules = new GameRulesForm();
+            gameRules.ShowDialog();
+        }
+
+        private void ïåðåçàïóñòèòüÈãðóToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void âûõîäToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
